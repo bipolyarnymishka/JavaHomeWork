6c3c1cb43dec09814fafc34a5802cab7ece33d1a
bipolyarnymishka <bipolyarnymishka@gmail.com>
2021-08-19 13:43:55 +0300

    Шестая домашка

---
+++ src/lesson_6.java
@@ -0,0 +1,9 @@
+public class lesson_6 {
+    public static void main(String[] args) {
+        Cat cat = new Cat(300, 3);
+        Dog dog = new Dog(150, 5);
+
+        cat.move();
+        dog.move();
+    }
+}
